913d57f0cc68a12524ce4ebd09d0666681b7a6dd
gamame <hikime@massdriver.com>
2023-01-24 00:33:38 +0900

    addsupabase

--- test/nuxt.config.ts
+++ test/nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
+
   nitro: {
     preset: "vercel-edge",
   },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    serviceKey: process.env.SUPABASE_SERVICE_KEY,
+    redirect: false,
+    cookies: {
+      name: "sb",
+    },
+  },
+  modules: [
+    "@nuxtjs/supabase",
+  ],
 })
