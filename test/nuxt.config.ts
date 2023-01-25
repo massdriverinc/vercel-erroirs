4d306184cc1311650821ee35e355887bb6cf98d1
gamame <hikime@massdriver.com>
2023-01-25 17:49:33 +0900

    remove preset vercel

--- test/nuxt.config.ts
+++ test/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  nitro: {
-    preset: "vercel-edge",
-  },
+  // nitro: {
+  //   preset: "vercel-edge",
+  // },
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
